d71e2de73d510e727fa2a88388416327c2c16155
Nicolás Pablo Castro Leal <npcastrol@gmail.com>
2022-09-24 20:23:04 -0300

    Change telescope find_files to default theme

--- lua/npcastro/keymaps.lua
+++ lua/npcastro/keymaps.lua
@@ -67,7 +67,8 @@ keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
 -- Telescope -- 
 -- keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
-keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
+--[[ keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = vim_buffer_cat }))<cr>", opts) ]]
+keymap("n", "<leader>f", "<cmd>Telescope find_files <cr>", opts)
 keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
 
 -- Nvimtree
